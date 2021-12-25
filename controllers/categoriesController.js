669df21f20d3713dcaabc1ba3c8992781c9726b1
Hung Duong Quang <qhungg289@protonmail.com>
2021-12-25 19:32:52 +0700

    Add controllers for create form

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,15 +1,5 @@
 const Category = require("../models/Category");
 
-const gradient = [
-	"linear-gradient(137deg, rgba(215,125,236,1) 0%, rgba(0,212,255,1) 100%)",
-	"linear-gradient(137deg, rgba(236,192,125,1) 0%, rgba(255,135,240,1) 100%)",
-	"linear-gradient(137deg, rgba(123,255,155,1) 0%, rgba(165,135,255,1) 100%)",
-	"linear-gradient(137deg, rgba(255,108,108,1) 0%, rgba(255,248,54,1) 100%)",
-	"linear-gradient(137deg, rgba(218,108,255,1) 0%, rgba(255,54,54,1) 100%)",
-	"linear-gradient(137deg, rgba(108,255,241,1) 0%, rgba(255,54,54,1) 100%)",
-	"linear-gradient(137deg, rgba(29,255,225,1) 0%, rgba(255,156,235,1) 100%)",
-];
-
 exports.categoriesListGet = (req, res, next) => {
 	Category.find({}).exec((err, categories) => {
 		if (err) {
@@ -19,7 +9,29 @@ exports.categoriesListGet = (req, res, next) => {
 		res.render("categoriesList", {
 			title: "Categories",
 			categories: categories,
-			gradient: gradient,
 		});
+	});
+};
+
+exports.categoriesCreateGet = (req, res, next) => {
+	res.render("categoryCreateForm", {
+		title: "Create new category",
+		nameInput: "",
+		descInput: "",
+	});
+};
+
+exports.categoriesCreatePost = (req, res, next) => {
+	const category = new Category({
+		name: req.body.name,
+		description: req.body.description,
+	});
+
+	category.save((err, result) => {
+		if (err) {
+			next(err);
+		}
+
+		res.redirect("/categories");
 	});
 };
