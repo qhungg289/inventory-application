ab22b455fac9e0ce69b4f10034d05159b1f3342a
Hung Duong Quang <qhungg289@protonmail.com>
2021-12-25 22:19:49 +0700

    Update item controller

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,5 @@
 const Item = require("../models/Item");
+const Category = require("../models/Category");
 
 exports.itemDetailGet = (req, res, next) => {
 	Item.findById(req.params.id)
@@ -10,4 +11,37 @@ exports.itemDetailGet = (req, res, next) => {
 
 			res.render("itemDetail", { title: result.name, item: result });
 		});
+};
+
+exports.itemCreateGet = (req, res, next) => {
+	Category.find({}).exec((err, result) => {
+		if (err) {
+			next(err);
+			return;
+		}
+
+		res.render("itemCreateForm", {
+			title: "Create new item",
+			categories: result,
+		});
+	});
+};
+
+exports.itemCreatePost = (req, res, next) => {
+	const item = new Item({
+		name: req.body.name,
+		description: req.body.description,
+		price: req.body.price,
+		numberInStock: req.body.stock,
+		category: req.body.category,
+	});
+
+	item.save((err, result) => {
+		if (err) {
+			next(err);
+			return;
+		}
+
+		res.redirect(result.url);
+	});
 };
