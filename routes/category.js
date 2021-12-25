d40d27cea20c08e1cd8ff9c95634dc3c1d9eef66
Hung Duong Quang <qhungg289@protonmail.com>
2021-12-25 19:43:09 +0700

    Add route for update category

--- routes/category.js
+++ routes/category.js
@@ -5,4 +5,12 @@ const categoryController = require("../controllers/categoryController");
 
 router.get("/:id", categoryController.categoryDetailGet);
 
+router.get("/:id/update", (req, res, next) => {
+	res.send(`GET_UPDATE_CATEGORY_FORM: ${req.params.id}`);
+});
+
+router.post("/:id/update", (req, res, next) => {
+	res.send(`POST_UPDATE_CATEGORY_FORM: ${req.params.id}`);
+});
+
 module.exports = router;
