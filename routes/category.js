88379b33cc8bca784dcb5af26cdb52c1c1c37789
Hung Duong Quang <qhungg289@protonmail.com>
2021-12-25 20:12:11 +0700

    Implement all the required route

--- routes/category.js
+++ routes/category.js
@@ -17,4 +17,12 @@ router.post("/:id/update", (req, res, next) => {
 	res.send(`POST_UPDATE_CATEGORY_FORM: ${req.params.id}`);
 });
 
+router.get("/:id/delete", (req, res, next) => {
+	res.send(`GET_DELETE_CATEGORY_FORM: ${req.params.id}`);
+});
+
+router.post("/:id/delete", (req, res, next) => {
+	res.send(`POST_DELETE_CATEGORY_FORM: ${req.params.id}`);
+});
+
 module.exports = router;
--- routes/item.js
+++ routes/item.js
@@ -3,6 +3,34 @@ const router = express.Router();
 
 const itemController = require("../controllers/itemController");
 
+// Create new item
+router.get("/create", (req, res, next) => {
+	res.send("get_new_item_form");
+});
+
+router.post("/create", (req, res, next) => {
+	res.send("post_new_item_form");
+});
+
+// Get item detail
 router.get("/:id", itemController.itemDetailGet);
+
+// Update item detail
+router.get("/:id/update", (req, res, next) => {
+	res.send("get_update_item_form");
+});
+
+router.post("/:id/update", (req, res, next) => {
+	res.send("post_update_item_form");
+});
+
+// Delete item
+router.get("/:id/delete", (req, res, next) => {
+	res.send("get_delete_item_form");
+});
+
+router.post("/:id/delete", (req, res, next) => {
+	res.send("post_delete_item_form");
+});
 
 module.exports = router;
